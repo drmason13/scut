f55d8b2e9b5fa72ace2cd192793ee6adef83bd23
drmason13 <drmason13@users.noreply.github.com>
2022-10-04 21:51:47 +0100

    document commands

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -11,7 +11,16 @@ mod upload;
 #[command(author, version, about, long_about = None)]
 pub(crate) enum Command {
     #[command(subcommand)]
+    /// Read or modify the current configuration file
+    ///
+    /// The configuration file is used to decide what to name your saves when uploading and which saves to download
     Config(ConfigCmd),
+    /// Ready a turn to be played
+    ///
+    /// Unzips the latest save file from your dropbox folder that isn't yours into your game saves folder
     Download(Download),
+    /// Share a turn that you've finished playing
+    ///
+    /// Zips your latest save from your game saves folder into your dropbox folder and names it accordingly
     Upload(Upload),
 }
