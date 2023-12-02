2e76739da0aef4ee02d810964e3f2a710941a3df
drmason13 <drmason13@users.noreply.github.com>
2023-12-02 14:14:26 +0000

    move autosave confirmation after upload/download
    TODO: make download/upload asynchronous

--- cli/src/command/mod.rs
+++ cli/src/command/mod.rs
@@ -29,32 +29,6 @@ pub fn run(
 
     let prediction = predictor.predict(side, player, turn_override, local, remote)?;
 
-    let predicted_autosave = match prediction.autosave {
-        AutosavePrediction::Ready(autosave) => if !ui.confirm(
-            &format!("Do you want to upload your autosave as: {autosave}?",),
-            Some(true),
-        ) {
-            Some(autosave)
-        } else {
-            None
-        },
-        AutosavePrediction::NotReady(autosave, reason) => match reason {
-            AutosavePredictionReason::AutosaveAlreadyUploaded => {
-                if ui.confirm(
-                    &format!("⚠️ {autosave} has already been uploaded. Do you want to overwrite it with your autosave? ⚠️",),
-                    Some(false),
-                ) {
-                    Some(autosave)
-                } else {
-                    None
-                }
-            },
-            AutosavePredictionReason::TeammateSaveNotUploaded => None,
-            AutosavePredictionReason::NewTurnAvailable => None,
-            AutosavePredictionReason::AutosaveNotAvailable => None,
-        }
-    };
-
     let mut confirmation_prompt = String::new();
 
     if !prediction.downloads.is_empty() {
@@ -79,51 +53,70 @@ pub fn run(
         }
     }
 
-    if let Some(ref autosave) = predicted_autosave {
-        write!(
-            confirmation_prompt,
-            "Will upload autosave as '{}' 🚀",
-            autosave
-        )?;
+    if !confirmation_prompt.is_empty() {
+        ui.message(&confirmation_prompt);
+
+        if !ui.confirm("Is that OK?", Some(true)) {
+            ui.wait_for_user_before_close("User cancelled. Stopping.");
+            return Ok(());
+        }
+
+        for save in prediction.downloads.iter() {
+            let download_path = local.location();
+            remote.download(save, download_path)?;
+        }
+
+        for save in prediction.uploads.iter() {
+            let local_path = local.locate_save(save)
+                .with_context(|| format!("No save file for '{}' exists in your local saved games folder!", &save))?
+                .ok_or_else(|| anyhow::anyhow!("scut predicted the need to upload your save '{}', but the corresponding file was not found!", &save))
+                .suggest("This may be a bug in scut! You can report issue to github: <https://github.com/drmason13/scut/issues/new>")?;
+
+            remote.upload(save, local_path.as_path())?;
+        }
     }
 
-    if confirmation_prompt.is_empty() {
+    if let Some(autosave) = match prediction.autosave {
+        AutosavePrediction::Ready(autosave) => if !ui.confirm(
+            &format!("Do you want to upload your autosave as: {autosave}?",),
+            Some(true),
+        ) {
+            Some(autosave)
+        } else {
+            None
+        },
+        AutosavePrediction::NotReady(autosave, reason) => match reason {
+            AutosavePredictionReason::AutosaveAlreadyUploaded => {
+                if ui.confirm(
+                    &format!("⚠️ {autosave} has already been uploaded. Do you want to overwrite it with your autosave? ⚠️",),
+                    Some(false),
+                ) {
+                    Some(autosave)
+                } else {
+                    None
+                }
+            },
+            AutosavePredictionReason::TeammateSaveNotUploaded => None,
+            AutosavePredictionReason::NewTurnAvailable => None,
+            AutosavePredictionReason::AutosaveNotAvailable => None,
+        }
+    } {
+        let local_path = local.locate_autosave()
+            .context("No autosave file exists in your local saved games folder!")?
+            .ok_or_else(|| anyhow::anyhow!("scut predicted the need to upload your autosave, but that file was not found!"))
+            .suggest("This may be a bug in scut! You can report the issue to github: <https://github.com/drmason13/scut/issues/new>")?;
+
+            ui.message(&format!("Uploading autosave as '{autosave}' 🚀"));
+        remote.upload(&autosave, local_path.as_path())?;
+
+    } else if confirmation_prompt.is_empty() {
+
         ui.message("Your local saves folder is synced with remote.");
         ui.wait_for_user_before_close("Nothing to do 💤");
         return Ok(());
-    }
 
-    ui.message(&confirmation_prompt);
-
-    if !ui.confirm("Is that OK?", Some(true)) {
-        ui.wait_for_user_before_close("User cancelled. Stopping.");
-        return Ok(());
-    }
-
-    for save in prediction.downloads.iter() {
-        let download_path = local.location();
-        remote.download(save, download_path)?;
-    }
-
-    for save in prediction.uploads.iter() {
-        let local_path = local.locate_save(save)
-            .with_context(|| format!("No save file for '{}' exists in your local saved games folder!", &save))?
-            .ok_or_else(|| anyhow::anyhow!("scut predicted the need to upload your save '{}', but the corresponding file was not found!", &save))
-            .suggest("This may be a bug in scut! You can report issue to github: <https://github.com/drmason13/scut/issues/new>")?;
-
-        remote.upload(save, local_path.as_path())?;
-    }
-
-    if let Some(autosave) = predicted_autosave {
-        let local_path = local.locate_autosave()
-        .context("No autosave file exists in your local saved games folder!")?
-        .ok_or_else(|| anyhow::anyhow!("scut predicted the need to upload your autosave, but that file was not found!"))
-        .suggest("This may be a bug in scut! You can report the issue to github: <https://github.com/drmason13/scut/issues/new>")?;
-
-        remote.upload(&autosave, local_path.as_path())?;
     }
 
     ui.wait_for_user_before_close("Done ✔️");
-
     Ok(())
 }
